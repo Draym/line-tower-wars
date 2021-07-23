24153f0ae34a39c89c6ccca83f61f57fc1b479ca
Kevin Andres <kevin.andres@stey.com>
2021-07-23 16:47:32 +0800

    init commit. base react-app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <div className="col-md-12">
+              <p className="title text-center">Welcome</p>
+          </div>
+          <div>
+              <p className="text-center">still nothing there, we are building stuff!</p>
+          </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
